06e31226f52b52d79354bf699c7e3dc88db142bb
Andres <andresperezperales@gmail.com>
2025-12-03 12:51:21 +0100

    manipulacion de directorios y archivos

---
+++ src/javaio/FileOperations.java
@@ -0,0 +1,48 @@
+package javaio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileOperations {
+public static void main(String[] args) {
+	File file = new File("/logs/user.log");
+	File directory= new File("/andres/logs/");
+	System.out.println("working directory:" + System.getProperty("user.dir"));
+	
+	createPath(directory);
+	
+	createFile(file);
+	//create directory using absolute path
+	File absoluteDir = new File(System.getProperty("user.dir") + "/logs/user.log");
+	absoluteDir.mkdirs();
+}
+
+private static void createPath(File file) {
+	// TODO Auto-generated method stub
+	System.out.println(file.isAbsolute());
+	System.out.println(file.getPath());
+	//create directories specific by the "file" object
+	file.getParentFile().mkdirs();
+	System.out.println(file.getAbsolutePath());
+}
+
+private static void createFile(File file) {
+	// TODO Auto-generated method stub
+	
+	if (file.exists()) {
+		System.out.println("the log file exists");
+	}
+	else {
+		System.out.println("the log file does not exist");
+		try {
+			file.createNewFile();
+		} 
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//With try catch, we can continue executing the program without interruption 
+		System.out.println("finished");
+	}
+}
+}
