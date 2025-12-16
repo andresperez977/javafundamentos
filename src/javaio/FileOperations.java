98310525791530192801d56b5cf76196f2bdcb7c
Andres <andresperezperales@gmail.com>
2025-12-16 13:15:59 +0100

    lectura de un archivo

--- src/javaio/FileOperations.java
+++ src/javaio/FileOperations.java
@@ -1,56 +1,112 @@
 package javaio;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileOperations {
-public static void main(String[] args) {
-	File file = new File("/logs/user.log");
-	File directory= new File("/andres/logs/");
-	System.out.println("working directory:" + System.getProperty("user.dir"));
-	
-	createPath(directory);
-	
-	createFile(file);
-	
-	createAbsolutePath();
-	//create directory using absolute path
-	File absoluteDir = new File(System.getProperty("user.dir") + "/logs/user.log");
-	absoluteDir.mkdirs();
-}
+	public static void main(String[] args) {
+		File file = new File("/logs/user.log");
+		File directory = new File("/andres/logs/");
+		System.out.println("working directory:" + System.getProperty("user.dir"));
 
-private static void createAbsolutePath() {
-	// TODO Auto-generated method stub
-	File absoluteDir = new File(System.getProperty("user.dir") + "/logs/user.log");
-	absoluteDir.mkdirs();  
-}
+		createPath(directory);
 
-private static void createPath(File file) {
-	// TODO Auto-generated method stub
-	System.out.println(file.isAbsolute());
-	System.out.println(file.getPath());
-	//create directories specific by the "file" object
-	file.getParentFile().mkdirs();
-	System.out.println(file.getAbsolutePath());
-}
+		createFile(file);
 
-private static void createFile(File file) {
-	// TODO Auto-generated method stub
-	
-	if (file.exists()) {
-		System.out.println("the log file exists");
+		createAbsolutePath();
+
+		writeFile();
+
+		readFile();
+		// create directory using absolute path
+		File absoluteDir = new File(System.getProperty("user.dir") + "/logs/user.log");
+		absoluteDir.mkdirs();
 	}
-	else {
-		System.out.println("the log file does not exist");
+
+	private static void readFile() {
+		// TODO Auto-generated method stub
 		try {
-			file.createNewFile();
-		} 
-		catch (IOException e) {
+			FileReader reader = new FileReader("log.txt");
+			reader.read();
+			System.out.println((char) reader.read());
+			System.out.println((char) reader.read());
+			System.out.println((char) reader.read());
+			System.out.println((char) reader.read());
+			System.out.println((char) reader.read());
+			// Check the condition first before executing the while body
+			int c ;
+			System.out.println((char)-1 == -1);
+			while ((c = reader.read()) != -1) {
+				System.out.println((char) c);
+
+			}
+			reader.close();
+			reader = new FileReader("log.txt");
+			// Execute the commands inside the "do" body first,
+			// then check the condition,if it is true ,repeat
+			// the execution
+			c=0;
+			System.out.println("the value of c is " + c);
+			do {
+				c=reader.read();
+				System.out.println((char) c);
+			} while (c!=-1);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//With try catch, we can continue executing the program without interruption 
-		System.out.println("finished");
 	}
-}
+
+	private static void writeFile() {
+		// TODO Auto-generated method stub
+		try {
+			FileWriter writer = new FileWriter("log.txt");
+			writer.write("Black holes");
+			writer.write("Pablo");
+			writer.write("t");
+
+			// Close the writer and refresh the data to the file
+			writer.close();
+			// writer.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void createAbsolutePath() {
+		// TODO Auto-generated method stub
+		File absoluteDir = new File(System.getProperty("user.dir") + "/logs/user.log");
+		absoluteDir.mkdirs();
+	}
+
+	private static void createPath(File file) {
+		// TODO Auto-generated method stub
+		System.out.println(file.isAbsolute());
+		System.out.println(file.getPath());
+		// create directories specific by the "file" object
+		file.getParentFile().mkdirs();
+		System.out.println(file.getAbsolutePath());
+	}
+
+	private static void createFile(File file) {
+		// TODO Auto-generated method stub
+
+		if (file.exists()) {
+			System.out.println("the log file exists");
+		} else {
+			System.out.println("the log file does not exist");
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// With try catch, we can continue executing the program without interruption
+			System.out.println("finished");
+		}
+	}
 }
