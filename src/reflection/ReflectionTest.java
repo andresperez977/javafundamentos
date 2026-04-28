c22ad68b8845fa221b10b8cec62ee9615038448a
Andres <andresperezperales@gmail.com>
2026-04-28 14:22:46 +0200

    imple

--- src/reflection/ReflectionTest.java
+++ src/reflection/ReflectionTest.java
@@ -5,10 +5,20 @@ import oopmodeling.addressbook.AddressBook;
 public class ReflectionTest {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		Class<AddressBook> cls = AddressBook.class;
+		Class<AddressBook> cls1 = AddressBook.class;
 		Class<AddressBook> cls2 = 
 				(Class<AddressBook>) new AddressBook().getClass();
 		Class<?> cls3=
 		Class.forName("oopmodeling.addressbook.AddressBook");
+		
+		inspectClass(cls1);
+	}
+
+	private static void inspectClass(Class<AddressBook> cls1) {
+		cls1.getDeclaredConstructors();
+		cls1.getDeclaredMethods();
+		cls1.getDeclaredFields();
+		cls1.getAnnotations();
+		
 	}
 }
